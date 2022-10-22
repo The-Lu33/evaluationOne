61042aaa40b70f6d4339580fdab11813ce78c1d3
The Lú <71472325+The-Lu33@users.noreply.github.com>
2022-10-22 10:31:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import QuotesBox from './components/QuotesBox'
+function App() {
+
+
+  return (
+   <div>
+    <QuotesBox/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/QuotesBox.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import quotes from "./quotes.json";
+
+const QuotesBox = () => {
+  // console.log(quotes[0]);
+
+  const [colors, setColors] = useState();
+  function generarNumero(numero) {
+    return (Math.random() * numero).toFixed(0);
+  }
+
+  function changeColors() {
+    setColors(
+      `rgb(${generarNumero(255)},${generarNumero(255)},${generarNumero(255)})`
+    );
+    return setColors;
+  }
+  //   console.log(colors);
+  const radomQuotes = Math.floor(Math.random() * quotes.length);
+  const [quoteIndex, setQuoteIndex] = useState(radomQuotes);
+  const changeIndex = () => {
+    setQuoteIndex(radomQuotes);
+    changeColors();
+  };
+
+  return (
+    <div className="container__box" style={{ background: [colors] }}>
+      <div className="quotesbox">
+        <span className="quotation" style={{ color: [colors] }}>
+          &#8220;
+        </span>
+        <p className="paragraph" style={{ color: [colors] }}>{quotes[quoteIndex].quote}</p>
+        
+        <h3 className="author" style={{ color: [colors] }}>
+          {quotes[quoteIndex].author}
+        </h3>
+        <button
+          className="btn"
+          onClick={changeIndex}
+          style={{ background: [colors] }}>
+          &#9758;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuotesBox;
